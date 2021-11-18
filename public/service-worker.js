173a237a89a4325460ce3b516a6776678fe40b00
Navleen <navleenkaurr@gmail.com>
2021-11-18 16:16:47 -0500

    Set up files to cache in service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/style.css",
+    "/assets/js/index.js",
+    "/assets/images/icon-192x192.png",
+    "/assets/images/icon-512x512.png",
+    "/manifest.webmanifest"
+];
