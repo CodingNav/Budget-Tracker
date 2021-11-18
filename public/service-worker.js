9ea12b015c351dfd45a8760965d198b70a2d785f
Navleen <navleenkaurr@gmail.com>
2021-11-18 18:05:47 -0500

    Fixed icon sizes

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,8 @@ const FILES_TO_CACHE = [
     "/assets/js/index.js",
     "/assets/images/icon-192x192.png",
     "/assets/images/icon-512x512.png",
-    "/manifest.webmanifest"
+    "/manifest.webmanifest",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -29,52 +30,49 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
     // takes control of client and removes old cache
     e.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
-  
+
     self.clients.claim();
-  });
+});
 
 // fetch
 self.addEventListener("fetch", (e) => {
     // cache successful request to the API
     if (e.request.url.includes("/api/")) {
-      e.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(e.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(e.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(e.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(e.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(e.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
     }
-  
+
     e.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(e.request).then(response => {
-          return response || fetch(e.request);
-        });
-      })
+        cache.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
     );
-  });
-  
+});
