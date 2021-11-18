20e4ac0b6ae13b9c6a5003686e9d46a7bf4ee82c
Navleen <navleenkaurr@gmail.com>
2021-11-18 16:27:50 -0500

    Set up service-worker activate event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,5 +26,21 @@ self.addEventListener("install", (e) => {
 });
 
 // activate
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
 
 // fetch
