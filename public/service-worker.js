1d2b258aeeffeb77dcb8270ace646a28be7e8d74
Navleen <navleenkaurr@gmail.com>
2021-11-18 16:23:55 -0500

    Set up service-worker install

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,24 @@ const FILES_TO_CACHE = [
     "/assets/images/icon-512x512.png",
     "/manifest.webmanifest"
 ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", (e) => {
+    // pre cache all static assets
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+});
+
+// activate
+
+// fetch
